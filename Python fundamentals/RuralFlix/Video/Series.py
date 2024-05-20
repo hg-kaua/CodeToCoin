8f939a4ff100f34fa02c2fb90eabf2ac9b36c67f
Hugo Kaua <hugokaua2002@gmail.com>
2024-05-20 20:19:39 -0300

    Implement class attribute

--- Python fundamentals/RuralFlix/Video/Series.py
+++ Python fundamentals/RuralFlix/Video/Series.py
@@ -1,1 +1,14 @@
+from Video.Video import Video
+
+class Series(Video):
+    
+    # class attribute
+    all = []
+    
+    def __init__(self, title, duration, launch='2000'):
+        super().__init__(title, duration, launch)
+        Series.all.append(self)
+
+    def __repr__(self):
+        return f"Serie( '{self.title}', '{self.duration}', '{self.duration}')"
 
