411698039db8e71b7e1d9e1189f652b64ad99929
Hugo Kaua <hugokaua2002@gmail.com>
2024-05-20 22:04:55 -0300

    Testing Encapsulation Principle

--- Python fundamentals/RuralFlix/RuralFlix.py
+++ Python fundamentals/RuralFlix/RuralFlix.py
@@ -5,39 +5,41 @@ from Video.Series import Series
 
 
 
-user_hugo = User('Hugo Medeiros', 'hugokaua2002@gmail.com', 'Hugo@2323', 'rua tantan', 55)
+# user_hugo = User('Hugo Medeiros', 'hugokaua2002@gmail.com', 'Hugo@2323', 'rua tantan', 55)
 
-user_hugo.verifyBilling()
-print(f'Your plan: {user_hugo.plan} \nYour credit: ${user_hugo.card}')
+# user_hugo.verifyBilling()
+# print(f'Your plan: {user_hugo.plan} \nYour credit: ${user_hugo.card}')
 
-user_hugo.changePlan()
-print(f'Your plan: {user_hugo.plan} \nYour credit: ${user_hugo.card}')
+# user_hugo.changePlan()
+# print(f'Your plan: {user_hugo.plan} \nYour credit: ${user_hugo.card}')
 
-video = Video('Homem de ferro', 100, 2008)
-
-
-video.play()
-video.advance()
-video.comeBack()
-video.comeBack()
-video.advance()
-
-batman = Film('Batman - Cavaleiro das trevas', 70, 2010)
-
-print(batman.title)
-batman.play()
-batman.stop()
-batman.advance()
-batman.advance()
-batman.advance()
-batman.advance()
-batman.comeBack()
+# video = Video('Homem de ferro', 100, 2008)
 
 
-series_data = Series('The flash', 40, 2014)
-series_data2 = Series('Suits', 40, 2016)
-series_data3 = Series('Greys anatomy', 40, 2004)
+# video.play()
+# video.advance()
+# video.comeBack()
+# video.comeBack()
+# video.advance()
 
-print(Series.all)
+# batman = Film('Batman - Cavaleiro das trevas', 70, 2010)
+
+# print(batman.title)
+# batman.play()
+# batman.stop()
+# batman.advance()
+# batman.advance()
+# batman.advance()
+# batman.advance()
+# batman.comeBack()
 
 
+series_data = Series('The flash', 42, 24, 8, 2014)
+
+series_data.seasons = 9
+
+print(series_data.seasons)
+
+series_data.seasons = 7
+
+print(series_data.seasons)
--- Python fundamentals/RuralFlix/Video/Series.py
+++ Python fundamentals/RuralFlix/Video/Series.py
@@ -5,10 +5,29 @@ class Series(Video):
     # class attribute
     all = []
     
-    def __init__(self, title, duration, launch='2000'):
+    def __init__(self, title, duration, episodes, seasons, launch='2000'):
         super().__init__(title, duration, launch)
+        self.__episodes = episodes
+        self.__seasons = seasons
+        
         Series.all.append(self)
 
+    
+    # implementing decoretes and getters and setters methods
+    
+    @property
+    def seasons(self):
+        return self.__seasons
+    
+    # setters
+    @seasons.setter
+    def seasons(self, value):
+        if value < self.__seasons:
+            raise Exception("Impossible to uptade sessons value")
+        else:
+            self.__seasons = value
+    
+    
     def __repr__(self):
-        return f"Serie( '{self.title}', '{self.duration}', '{self.duration}')"
+        return f"Serie( '{self.title}', '{self.duration}min', '{self.__episodes}', '{self.__seasons}', '{self.launch}')"
 
